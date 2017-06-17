eb162d1bcc92737805a1463ad3f639ea5ab54e5a
Growiel <girardol@gmail.com>
2017-06-18 00:09:01 +0900

    second commit

---
+++ Source/OurSecondGame/OurSecondGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OurSecondGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OURSECONDGAME_API AOurSecondGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
